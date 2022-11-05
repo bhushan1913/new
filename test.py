9569c58473f03577b47551d3071102c5c0da9e57
bhushan <bhushanrekhapatil@gmail.com>
2022-11-06 04:55:24 +0530

    adding new one

---
+++ test.py
@@ -0,0 +1,15 @@
+#with open(path, 'rb') as file:
+#    pickle.load
+import numpy as np
+import pickle
+
+Iris_model = pickle.load(open("Iris_model.pickle","rb"))
+
+def predict_species(SepalLengthCm,SepalWidthCm, PetalLengthCm, PetalWidthCm):
+    x = np.zeros(4)
+    x[0] = SepalLengthCm
+    x[1] = SepalWidthCm
+    x[2] = PetalLengthCm
+    x[3] = PetalWidthCm
+    species= Iris_model.predict([x])[0]
+    return species
